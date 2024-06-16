1d785620c0187dcf68832fdb319924a978d2435a
Christian Cruz <christian.cruz92@hotmail.com>
2024-06-16 14:33:30 -0400

    adding initial files for eightball app

---
+++ src/EightBall.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./EightBall.css"
+import "./index.css"
+
+
+// function handleClick() {
+//   console.log("BrokenClick clicked!");
+// }
+
+function EightBall() {
+  return (
+    <div className="circle"> </div>
+  );
+}
+
+export default EightBall;
