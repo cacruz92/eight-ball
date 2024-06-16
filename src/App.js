c92f96baa050455af835750bff90dbacbe022772
Christian Cruz <christian.cruz92@hotmail.com>
2024-06-16 16:42:37 -0400

    added EightBall and title components with functionality using state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import EightBall from "./EightBall"
+import Title from "./Title";
 
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <EightBall />
     </div>
   );
---
+++ src/Title.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./EightBall.css"
+import "./index.css"
+
+
+
+const Title = () => {
+  return (
+   <h1>Magic 8-Ball</h1>
+  );
+}
+
+export default Title;
